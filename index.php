ff3aa6c357d9e6e9e588e790479a553f743800af
Alejandro santalla <alejandrosantalla@MacBook-Pro-de-Alejandro.local>
2022-08-28 22:48:05 +0200

    prueba portatil

--- index.php
+++ index.php
@@ -3,31 +3,38 @@
 
     
 <main >
-    <div class=" container d-flex">
-        <div class="container-fluid">
+    <div class="container ">
 
+        <div class="container-fluid d-sm-flex d-lg-inline-flex">
+            
+                <div class="row banner-text px-4">
 
-            <div class="container group-banner d-md-flex justify-content-center align-items-center ">
-                <div class="col-12 col-md-6 ">
-                            <div class="px-2">
-                                <figure>
-                                    <img class="img-fluid" src="images/bullet2.png" alt="image-header" >
-                                </figure>
-
-                            </div>
+                    <div class="group-text ">
+                            <h1 class="font-weight-bold">Automatiza la estancia de tus huéspedes</h1>
+                            <p> La única herramienta que te permite ahorrar tiempo y dinero, automatizando la estnacia de tus huéspedes desde la reserva, ¡Hasta el checkout! </p>
+                            
+                    </div>
+                    <div class="buttons-banner">
+                            <button class="btn-mn-primary"> Regístrate Ahora!</button>
+                            <button class="btn-rounded"> Reservar una demo</button>
+                    </div>
                 </div>
-            <div class="banner-text px-4">
 
-                <div class="group-text md:mt-4 col-12 col-md-8 col-lg-12">
-                        <h1 class="font-weight-bold">Automatiza la estancia de tus huéspedes</h1>
-                        <p> La única herramienta que te permite ahorrar tiempo y dinero, automatizando la estnacia de tus huéspedes desde la reserva, ¡Hasta el checkout! </p>
+             
+            <div class="container group-banner justify-content-center align-items-center ">
+                
+            <div class="row-justify-content-md-center">
+
+                <div class="col col-12 col-md-6 ">
+                                
+                                    <figure>
+                                        <img class="img-fluid" src="images/bullet2.png" alt="image-header" >
+                                    </figure>
+    
                         
-                </div>
-                <div class="buttons-banner">
-                        <button class="btn-mn-primary"> Regístrate Ahora!</button>
-                        <button class="btn-rounded"> Reservar una demo</button>
-                </div>
+                    </div>
             </div>
+       
        
                     
             </div>

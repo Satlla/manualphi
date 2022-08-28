00fac6228f190a0d21c82ec0b510ee513a5b0a7d
Alejandro Santalla <alejandrosantalla@iMac-de-Alejandro.local>
2022-08-28 19:53:11 +0200

    primer repo

---
+++ includes/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/estilos.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
+
+
+
+<style>
+        @media (min-width: 992px) {
+
+            .alto-100{
+                height: 100vh;
+            }
+            
+        }
+    </style>
+</head>
+<body>
+
+<nav class="menu navbar navbar-light">
+  <div class="container d-flex">
+    <a class="navbar-brand" href="#">
+      <img src="images/logomanualphi.svg" alt="logo">
+    </a>
+
+
+<button class="btn-rounded " data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+  <span class="d-flex">
+    <img src="images/list.svg" alt="">
+    <img src="images/user-circle.svg" alt="">
+
+  </span>
+
+
+
+      </button>
+
+
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel"> <img src="images/logomanualphi.svg" alt="logo"></h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+    </div>
+    <div class="dropdown mt-3">
+      <button class="btn-mn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+        Language
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <li><a class="dropdown-item" href="#">Español</a></li>
+        <li><a class="dropdown-item" href="#">Inglés</a></li>
+        <li><a class="dropdown-item" href="#">Francés</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+    
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php include ('./includes/header.php') ?>
+
+
+    
+<main >
+    <div class=" container d-flex">
+        <div class="container-fluid">
+
+
+            <div class="container group-banner d-md-flex justify-content-center align-items-center ">
+                <div class="col-12 col-md-6 ">
+                            <div class="px-2">
+                                <figure>
+                                    <img class="img-fluid" src="images/bullet2.png" alt="image-header" >
+                                </figure>
+
+                            </div>
+                </div>
+            <div class="banner-text px-4">
+
+                <div class="group-text md:mt-4 col-12 col-md-8 col-lg-12">
+                        <h1 class="font-weight-bold">Automatiza la estancia de tus huéspedes</h1>
+                        <p> La única herramienta que te permite ahorrar tiempo y dinero, automatizando la estnacia de tus huéspedes desde la reserva, ¡Hasta el checkout! </p>
+                        
+                </div>
+                <div class="buttons-banner">
+                        <button class="btn-mn-primary"> Regístrate Ahora!</button>
+                        <button class="btn-rounded"> Reservar una demo</button>
+                </div>
+            </div>
+       
+                    
+            </div>
+        </div>
+
+
+
+                
+    </div>
+
+
+  
+</main>
+
+<script src="libs/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>

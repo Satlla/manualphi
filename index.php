71261a065025b3a80d220d112065b0a27c7322ad
Alejandro Santalla <alejandrosatlla@gmail.com>
2022-08-31 10:48:36 +0200

    image banner
    image-banner

--- index.php
+++ index.php
@@ -5,31 +5,31 @@
 <main >
     <div class="container ">
 
-        <div class="container-fluid d-sm-flex d-lg-inline-flex">
+        <div class="container-fluid d-sm-flex mt-5">
             
-                <div class="row banner-text px-4">
+                <div class="row banner-text align-items-center  ">
 
-                    <div class="group-text ">
-                            <h1 class="font-weight-bold">Automatiza la estancia de tus huéspedes</h1>
+                    <div class="  group-text ">
+                            <h1 class="title-1">Automatiza la estancia de tus huéspedes</h1>
                             <p> La única herramienta que te permite ahorrar tiempo y dinero, automatizando la estnacia de tus huéspedes desde la reserva, ¡Hasta el checkout! </p>
                             
-                    </div>
-                    <div class="buttons-banner">
-                            <button class="btn-mn-primary"> Regístrate Ahora!</button>
-                            <button class="btn-rounded"> Reservar una demo</button>
+                            <div class="d-flex justify-content-start ">
+                                    <button class="btn-mn-primary"> Regístrate!</button>
+                                    <button class="btn-rounded ms-4"> Reservar una demo</button>
+                            </div>
                     </div>
                 </div>
 
              
-            <div class="container group-banner justify-content-center align-items-center ">
+            <div class="container group-banner  ">
                 
-            <div class="row-justify-content-md-center">
+            <div class="row ">
 
-                <div class="col col-12 col-md-6 ">
+                <div class=" col-12 d-flex justify-content-center align-items-center ">
                                 
-                                    <figure>
-                                        <img class="img-fluid" src="images/bullet2.png" alt="image-header" >
-                                    </figure>
+                                    <div class="text-center">
+                                        <img class=" mt-3 img-fluid" src="images/bullet2.png" alt="image-header" >
+                                    </div>
     
                         
                     </div>
@@ -43,6 +43,50 @@
 
 
                 
+    </div>
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+            <div class="col-1 border">1</div>
+
+        </div>
+        <div class="row">
+            <div class="col-4 border">1</div>
+            <div class="col-4 border">1</div>
+            <div class="col-4 border">1</div>
+
+
+        </div>
+        <div class="row">
+            <div class="col-3 border">1</div>
+            <div class="col-3 border">1</div>
+            <div class="col-3 border">1</div>
+            <div class="col-3 border">1</div>
+           
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                6
+            </div>
+            <div class="col-6">
+                6
+            </div>
+        </div>
     </div>
 
 

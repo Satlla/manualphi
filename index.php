5605539604e0b3009c97afa7d588d698ae8d0025
Alejandro Santalla <alejandrosatlla@gmail.com>
2022-09-06 11:17:35 +0200

    viñetas landing

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             
                 <div class="row banner-text align-items-center  ">
 
-                    <div class="  group-text ">
+                    <div class=" group-text ">
                             <h1 class="title-1 ">Automatiza la estancia de tus huéspedes</h1>
                             <p> La única herramienta que te permite ahorrar tiempo y dinero, automatizando la estnacia de tus huéspedes desde la reserva, ¡Hasta el checkout! </p>
                             
@@ -25,9 +25,9 @@
 
                 <div class=" col-12 d-flex justify-content-center align-items-center ">
                                 
-                                    <div class="text-center">
-                                        <img class=" mt-3 img-fluid" src="images/bullet2.png" alt="image-header" >
-                                    </div>
+                    <div class="text-center">
+                        <img class=" mt-3 img-fluid" src="images/bullet2.png" alt="image-header" >
+                    </div>
     
                         
                     </div>
@@ -36,22 +36,22 @@
             </div>
         </div>
 
-        <div class=" container-purple mt-4">
-                <div class="row d-flex justify-content-around">
+        <div class=" text-purple mt-4">
+                <div class="row d-flex justify-content-center">
             
-                    <div class="col-3  ">
+                    <div class="col-3 text-center ">
                         <h1 class="">23M</h1>
                         <p class="lead">Scans.</p>
                     </div>
-                    <div class="col-3 ">
+                    <div class="col-3 text-center ">
                         <h1 class="">4M</h1>
                         <p class="lead">Hosts.</p>
                     </div>
-                    <div class="col-3 d-none d-sm-block ">
+                    <div class="col-3 text-center d-none d-sm-block ">
                         <h1 class="">98%</h1>
                         <p class="lead">Sastisfaction.</p>
                     </div>
-                    <div class="col-3 d-none d-sm-none d-md-block ">
+                    <div class="col-3 text-center d-none d-sm-none d-md-block ">
                         <h1 class="">12k</h1>
                         <p class="lead">reviews</p>
                     </div>
@@ -71,37 +71,37 @@
     </div>
     <div class="acommodations d-flex justify-content-center align-items-center w-100">
         <div class="row d-block d-md-flex">
-            <div class="card col-4 m-3" style="width: 18rem;">
+            <div class="card col-4 m-3 p-4 " style="width: 18rem;">
                 <img src="images/viñeta 1 .svg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                <div class="card-body text-center">
+                    <h5 class="card-title">Hoteles</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn-rounded">Go somewhere</a>
                 </div>
             </div>
 
-            <div class="card col-4 m-3" style="width: 18rem;">
+            <div class="card col-4 m-3 p-4" style="width: 18rem;">
                 <img src="images/viñeta2.svg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                <div class="card-body text-center">
+                    <h5 class="card-title">Apartamentos</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn-rounded">Go somewhere</a>
                 </div>
             </div>
 
-            <div class="card col-4 m-3" style="width: 18rem;">
+            <div class="card col-4 m-3 p-4" style="width: 18rem;">
                 <img src="images/viñeta3.svg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                <div class="card-body text-center">
+                    <h5 class="card-title">Habitaciones</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class=" btn-rounded ">Go somewhere</a>
                 </div>
             </div>
 
-            <div class="card col-4 m-3" style="width: 18rem;">
+            <div class="card col-4 m-3 p-4" style="width: 18rem;">
                 <img src="images/viñeta3.svg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                <div class="card-body text-center">
+                    <h5 class="card-title">Campings</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn-rounded">Go somewhere</a>
                 </div>
@@ -112,25 +112,38 @@
         </div>
 
         
+        
+        
+    </div>
+    <div class=" w-100 d-flex justify-content-center text-center m-4" >
+
+        <a href="#" class=" btn-mn-primary ">Prueba Manualphi Ahora </a>
+    </div>
+
+    <div class="header-purple-gradient mt-4 w-100 bt-1">
+        <div class="container w-80">
+    
+            <p class="text-center  fs-1 "> Hemos desarrollado una herramienta con la que ahorrarás cientos de horas al año</p>
+            <p class="text-center fw-light fs-4 "> Descubre los manuales interactivos para huéspedes </p>
+
+        
+        </div>
 
     </div>
 
 
-    <div class="row d-flex justify-content-center align-items-center">
+    <div class="row d-sm-flex d-md-block justify-content-center align-items-center w-100 mt-4">
         <div class="row">
 
-            <div class="col-3">
-              One of two columns
+            <div class="col-6 text-center">
+              <img src="images/image-header.png" alt="" width="300">
             </div>
-            <div class="col-3">
-              One of two columns
+            <div class="col-6 text-center">
+              <h3 class=""> Video-Manual de tu alojamiento  </h3>
+              <p class="text-center fw-light fs-4 lh-0"> Haz un video de cómo funciona el aparato o zona que deseas explicar a tu huésped</p>
+
             </div>
-            <div class="col-3">
-              One of two columns
-            </div>
-            <div class="col-3">
-              One of two columns
-            </div>
+            
         </div>
   </div>
   

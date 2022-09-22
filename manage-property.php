aac41bb7dcbfb24087f859bdc0f22bfcf7bb94fc
Alejandro santalla <alejandrosantalla@MacBook-Pro-de-Alejandro.local>
2022-09-22 18:23:27 +0200

    tabla de manage-property empezada

--- manage-property.php
+++ manage-property.php
@@ -1,17 +1,77 @@
 <?php include ('./includes/header-dashboard.php') ?>
 
 <div class="container-dashboard mx-auto mt-4 ps-2 ">
-    <h1 class="title-2">Hola Barriot</h1>
+    <h1 class="title-2">La Casa Azul</h1>
     <p> Este es tu panel de control, desde aquí podrás agregar propiedades y generar los manuales que tus huéspedes necesitan!</p>
     <div class="container-dashboard mx-auto mt-4  ">
         <div class=" d-flex justify-content-between align-items-center p-2 ">
-        
-            <button class="add-button"> Añadir Zona <img class="plus-icon" src="images/plus.svg" alt=""> </button>
+            <a href="dashboard.php"class="add-button"><img class="plus-icon" src="icons/chevron-left.svg" alt="">  Volver </a>
+            <a class="add-button"> Añadir Zona <img class="plus-icon" src="images/plus.svg" alt=""> </a>
 
 
         </div>
 
     </div>
+</div>
+
+<div class="container-dashboard mx-auto mt-4 ps-2 ">
+
+<table class="table">
+  <thead>
+    <tr>
+      <th class="text-center" scope="col"></th>
+      <th class="text-left" scope="col">Zona</th>
+      <th class="text-center" scope="col">Editar</th>
+      <th class="text-center" scope="col">Eliminar</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Check in</td>
+      <td class="text-center"> <img src="icons/pencil-alt.svg" alt=""></td>
+      <td class="text-center"> <img src="icons/x.svg" alt=""></td>
+
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Climatización</td>
+      <td class="text-center"> <img src="icons/pencil-alt.svg" alt=""></td>
+      <td class="text-center"> <img src="icons/x.svg" alt=""></td>
+
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Vitrocerámica</td>
+      <td class="text-center"> <img src="icons/pencil-alt.svg" alt=""></td>
+      <td class="text-center"> <img src="icons/x.svg" alt=""></td>
+
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Check out</td>
+      <td class="text-center"> <img src="icons/pencil-alt.svg" alt=""></td>
+      <td class="text-center"> <img src="icons/x.svg" alt=""></td>
+
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Chimenea</td>
+      <td class="text-center"> <img src="icons/pencil-alt.svg" alt=""></td>
+      <td class="text-center"> <img src="icons/x.svg" alt=""></td>
+    </tr>
+    <tr>
+      <th scope="row ">3</th>
+      <td>Cafetera</td>
+      <td class="text-center"> <img src="icons/pencil-alt.svg" alt=""></td>
+      <td class="text-center"> <img src="icons/x.svg" alt=""></td>
+    </tr>
+  </tbody>
+</table>
+
+
 </div>
 
 
